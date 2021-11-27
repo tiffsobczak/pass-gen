76362160270e8ce72f51865532ad8a4a9943e5b0
Tiffany <tiff.sobczak@gmail.com>
2021-11-27 12:04:55 -0600

    generates password

--- friendly-parakeet/Develop/script.js
+++ friendly-parakeet/Develop/script.js
@@ -1,32 +1,78 @@
 // Assignment code here
+const LOWER_CASE = "abcdefghijklmnopqurstuvwxyz";
+const UPPER_CASE = "ABCDEFGHIJKLMNOPQURSTVWXYZ";
+const NUMBERS = "0123456789";
+const SYMBOLS = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var lengthInput = window.prompt('How many characters do you want your password to include?');
-  var length = parseInt(lengthInput);
-  var isInvalidLength = length <= 8 || length >= 128;
+  const lengthInput = window.prompt('How many characters do you want your password to include?');
+  const length = parseInt(lengthInput);
+  const isInvalidLength = length < 8 || length > 128;
 
   if (isInvalidLength) {
     window.alert('Length must between 8 and 128');
     return;
   }
 
-  var isLowerAlpha = window.confirm('Do you want to include lowercase letters?')  
-  var isUpperAlpha = window.confirm('Do you want to include uppercase letters?')  
-  var isNumeric = window.confirm('Do you want to include numbers?')  
-  var isSpecialChar = window.confirm('Do you want to include special characters?')  
-  var isValidCharacterType = isLowerAlpha || isUpperAlpha || isNumeric || isSpecialChar;
+  const isLowerAlpha = window.confirm('Do you want to include lowercase letters?')  
+  const isUpperAlpha = window.confirm('Do you want to include uppercase letters?')  
+  const isNumeric = window.confirm('Do you want to include numbers?')  
+  const isSpecialChar = window.confirm('Do you want to include special characters?')  
+  const isValidCharacterType = isLowerAlpha || isUpperAlpha || isNumeric || isSpecialChar;
 
   if (!isValidCharacterType) {
     window.alert('Password must include at least one character type.');
     return;
   }
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword(length, isLowerAlpha, isUpperAlpha, isNumeric, isSpecialChar);
+  const passwordText = document.querySelector("#password");
+  passwordText.innerText = password;
+}
+
+function generatePassword(size, isLower, isUpper, isNumberic, isSpecial) {
+  let password = "";
+
+  while (password.length < size) {
+    if (isLower) {
+      // add a random lower case char
+      const index = getRandomInt(0, 25);
+      password = password + LOWER_CASE[index];
+    }
+
+    if (isUpper) {
+      // add a random upper case char
+      const index = getRandomInt(0, 25);
+      password = password + UPPER_CASE[index];
+    }
+
+    if (isNumberic) {
+      // add a random number
+      const index = getRandomInt(0, 9);
+      password = password + NUMBERS [index];
+    }
+
+    if (isSpecial) {
+      // add a random special char
+      const index = getRandomInt(0, 29);
+      password = password + SYMBOLS [index];
+    }
+  }
+
+  // size the password
+  password = password.substring(0, size);
+
+  return password;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 
