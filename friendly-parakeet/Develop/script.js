e577d59e2f0e51204b73f509ad43f7fe8e20ad81
Tiffany <tiff.sobczak@gmail.com>
2021-11-27 11:22:51 -0600

    Created prompts

--- friendly-parakeet/Develop/script.js
+++ friendly-parakeet/Develop/script.js
@@ -1,37 +1,34 @@
 // Assignment code here
-const alpha= "abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQURSTVWXYZ";
-const numbers="0123456789";
-const symbols="!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-
-const passwordTxt=document.getElementById("password");
-const length=document.getElementById("length");
-const incNumbers=document.getElementById("numbers");
-const inclSymbols=document.getElementById("symbols");
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var lengthInput = window.prompt('How many characters do you want your password to include?');
+  var length = parseInt(lengthInput);
+  var isInvalidLength = length <= 8 || length >= 128;
+
+  if (isInvalidLength) {
+    window.alert('Length must between 8 and 128');
+    return;
+  }
+
+  var isLowerAlpha = window.confirm('Do you want to include lowercase letters?')  
+  var isUpperAlpha = window.confirm('Do you want to include uppercase letters?')  
+  var isNumeric = window.confirm('Do you want to include numbers?')  
+  var isSpecialChar = window.confirm('Do you want to include special characters?')  
+  var isValidCharacterType = isLowerAlpha || isUpperAlpha || isNumeric || isSpecialChar;
+
+  if (!isValidCharacterType) {
+    window.alert('Password must include at least one character type.');
+    return;
+  }
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  const alpha= "abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQURSTVWXYZ";
-  const numbers="0123456789";
-  const symbols="!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-  let password =''
-  for (var i=0; i < 8; i++) {
-    let generate = letters[Math.floor(Math.random()*128)]
-  }
-  passwordText.value = password;
-
 }
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); {
-  let characters = alpha;
-  incNumbers.checked 
-}
-
+generateBtn.addEventListener("click", writePassword); 
